44589874e1c7e4a1ca124b1b203ef24d9db5f266
Reto Gmür <me@farewellutopia.com>
2017-11-26 15:03:17 +0000

    autcompletes genus.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,5 @@
 /* global Mustache */
-
-function getNewTaxon(genus, species) {
-    let query = "PREFIX treat: <http://plazi.org/vocab/treatment#>" +
-            "PREFIX dwc: <http://rs.tdwg.org/dwc/terms/>" +
-            "SELECT * WHERE { " +
-            "  ?treatment (treat:augmentsTaxonConcept|treat:definesTaxonConcept) ?newTaxon ." +
-            "    ?treatment treat:deprecates ?oldTaxon ." +
-            "    ?oldTaxon dwc:genus \"" + genus + "\"." +
-            "    ?oldTaxon dwc:species \"" + species + "\"." +
-            "    ?newTaxon dwc:genus ?newGenus." +
-            "    ?newTaxon dwc:species ?newSpecies." +
-            "} ";
+function executeSparql(query) {
     let encodedQuery = encodeURIComponent(query);
     return fetch("https://test.lindas-data.ch/sparql?query=" + encodedQuery, {
         headers: {
@@ -23,7 +12,20 @@ function getNewTaxon(genus, species) {
         } else {
             return response.json();
         }
-    }).then(json => {
+    });
+}
+function getNewTaxon(genus, species) {
+    let query = "PREFIX treat: <http://plazi.org/vocab/treatment#>" +
+            "PREFIX dwc: <http://rs.tdwg.org/dwc/terms/>" +
+            "SELECT * WHERE { " +
+            "  ?treatment (treat:augmentsTaxonConcept|treat:definesTaxonConcept) ?newTaxon ." +
+            "    ?treatment treat:deprecates ?oldTaxon ." +
+            "    ?oldTaxon dwc:genus \"" + genus + "\"." +
+            "    ?oldTaxon dwc:species \"" + species + "\"." +
+            "    ?newTaxon dwc:genus ?newGenus." +
+            "    ?newTaxon dwc:species ?newSpecies." +
+            "} ";
+    return executeSparql(query).then(json => {
         //json.results.bindings[0].oldTaxon.value
         return json.results.bindings.map(binding => {
             let result = {};
@@ -42,7 +44,7 @@ function getNewTaxon(genus, species) {
 function showReport(genus, species) {
     let expandedTaxa = {};
     function appendDeprecations(genus, species) {
-        let currentAcceptedName = "";    
+        let currentAcceptedName = "";
         getNewTaxon(genus, species).then(deprecations => {
             var template = $('#newTaxonTpl').html();
             deprecations.forEach(deprecation => {
@@ -50,8 +52,8 @@ function showReport(genus, species) {
                 $('#report').append(html);
             });
             deprecations.forEach(deprecation => {
-                if (!expandedTaxa[deprecation.newTaxon.genus+deprecation.newTaxon.species]) {
-                    expandedTaxa[deprecation.newTaxon.genus+deprecation.newTaxon.species] = true;
+                if (!expandedTaxa[deprecation.newTaxon.genus + deprecation.newTaxon.species]) {
+                    expandedTaxa[deprecation.newTaxon.genus + deprecation.newTaxon.species] = true;
                     appendDeprecations(deprecation.newTaxon.genus, deprecation.newTaxon.species);
                 }
             });
@@ -66,3 +68,34 @@ $("#lookup").on("click", e => {
     return false;
 });
 
+let input = document.getElementById("genus");
+let previousValue = input.value;
+input.onkeyup = (e) => {
+    if (input.value.length >= 2) {
+        if ((e.key !== "Enter") && (input.value !== previousValue)) {
+            previousValue = input.value;
+            getGenusSuggestions(input.value).then(values => awesomplete.list = values);
+        }
+    }
+    return true;
+};
+let awesomplete = new Awesomplete(input);
+
+/**
+ * 
+ * @param {type} prefix
+ * @returns A promise for an array of matching genera
+ */
+function getGenusSuggestions(prefix) {
+    let query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
+                "PREFIX dwc: <http://rs.tdwg.org/dwc/terms/>\n"+
+                "PREFIX dwcfp: <http://filteredpush.org/ontologies/oa/dwcFP#>\n"+
+                "SELECT DISTINCT ?genus WHERE {\n"+
+                "?sub dwc:genus ?genus .\n"+
+                "?sub rdf:type dwcfp:TaxonName.\n"+
+                "FILTER REGEX(?genus, \"^"+prefix+"\",\"i\")\n"+
+            "} ORDER BY UCASE(?genus) LIMIT 15";
+    return executeSparql(query).then(json => {
+        return json.results.bindings.map(binding => binding.genus.value);
+    });
+}
