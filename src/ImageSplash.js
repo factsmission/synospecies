a92ef78920ce140e4094795364f82d226424ccbe
nleanba <noam@factsmission.com>
2019-12-08 11:56:42 +0100

    styled images

--- src/ImageSplash.js
+++ src/ImageSplash.js
@@ -4,12 +4,30 @@ export default class ImageSplash {
         this._element = element;
         this._taxaManager = taxaManager;
         this.addedImages = {};
-        this.template = `
-        <figure>
-            <img src="{{url}}" alt="{{description}}" style="width:100%; max-width:600px;" />
-            <figcaption>{{description}}</figcaption>
-        </figure>
-        `;
+        this.to_html = (image) => {
+            const desc = image.description || '';
+            const figure = document.createElement('figure');
+            const img = document.createElement('img');
+                  img.setAttribute('src', image.url);
+                  img.setAttribute('alt', desc);
+            const figcaption = document.createElement('figcaption')
+            if (desc.length > 400) {
+                figcaption.innerHTML = desc.slice(0, 401) + '<span hidden aria-hidden="false">' + desc.slice(401) + '</span>';
+                const expandbtn = document.createElement("span");
+                      expandbtn.innerHTML = "...";
+                      expandbtn.classList.add("expandbtn");
+                      expandbtn.addEventListener("click", e => {
+                        figcaption.innerHTML = desc;
+                        e.preventDefault();
+                      });
+                figcaption.append(expandbtn);
+            } else {
+                figcaption.textContent = desc;
+            }
+            figure.append(img);
+            figure.append(figcaption);
+            return figure;
+        };
     }
 
     appendImages(taxon) {
@@ -17,8 +35,7 @@ export default class ImageSplash {
             images.forEach(image => {
                 if (!this.addedImages[image.url]) {
                     this.addedImages[image.url] = true;
-                    var html = Mustache.to_html(this.template, image);
-                    this._element.innerHTML += html;
+                    this._element.append(this.to_html(image));
                 }
             });
         });
