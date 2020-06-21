6f2e76184033f6765226033d30c02ad1eca8bb3a
Reto Gmür <reto@factsmission.com>
2020-06-21 19:33:17 +0200

    getting endpoint from localstorage

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const endpoint = function () {
-  return 'https://trifid-lindas.test.cluster.ldbar.ch/query' // 'https://treatment.ld.plazi.org/sparql'
+  const localValue = localStorage.getItem('plazi-treatments-endpoint')
+  return localValue || 'https://trifid-lindas.test.cluster.ldbar.ch/query' // 'https://treatment.ld.plazi.org/sparql'
 }
 export default {
   endpoint
