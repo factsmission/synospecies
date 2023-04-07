469288d6f655c4dce1e64c9d99e73aa4bf62c2bb
nleanba <25827850+nleanba@users.noreply.github.com>
2023-04-07 17:56:52 +0000

    Approaches #99

---
+++ src/components/CitedMaterials.vue
@@ -0,0 +1,128 @@
+<template>
+  <div v-if="mcs.length > 0">
+    {{ mcs.length }} specimens referenced
+    <button @click="open = !open">
+      [{{ open ? "hide" : "show" }}]
+    </button>
+    {{ mcs.find(mc => mc.typeStatus?.toLocaleLowerCase().includes('holotype')) ? '(incl. holotype)' : '' }}
+    <table v-if="open">
+      <tr v-for="mc in mcs" :key="JSON.stringify(mc)" :title="pretty(mc)">
+        <td :class="`mc_type ${mc.typeStatus?.toLocaleLowerCase().includes('holotype') ? 'green' : ''}`">
+          {{ mc.typeStatus?.[0].toUpperCase() }}
+        </td>
+        <td class="mc_code">
+          {{ mc.collectionCode }}:
+          {{ mc.catalogNumber }}
+        </td>
+        <td v-if="mc.eventDate" class="mc_date">
+          ({{ date(mc) }})
+        </td>
+        <td v-else />
+        <td class="mc_links">
+          <a v-if="mc.httpUri && (!mc.gbifSpecimenId || mc.httpUri.endsWith(mc.gbifSpecimenId)) && (!mc.gbifOccurrenceId || mc.httpUri.endsWith(mc.gbifOccurrenceId))"
+            :href="mc.httpUri">Link ➶</a>
+        </td>
+        <td v-if="mc.gbifSpecimenId || mc.gbifOccurrenceId" class="mc_links">
+          <small>GBIF:</small> <a v-if="mc.gbifSpecimenId"
+            :href="'https://www.gbif.org/specimen/' + mc.gbifSpecimenId">Specimen</a> <a v-if="mc.gbifOccurrenceId"
+            :href="'https://www.gbif.org/occurrence/' + mc.gbifSpecimenId">Occurrence</a>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="5" style="text-align: center;"><small><i>hover over a row for more details</i></small></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import type { MaterialCitation } from '@factsmission/synogroup';
+
+function unCamelCase(string: string) {
+  const result = string.charAt(0).toUpperCase() + string.slice(1).replace(/([A-Z])/g, ' $1').trim();
+  return result.replace(/gbif/gi, "GBIF").replace(/id/gi, "ID").replace(/http/gi, "HTTP").replace(/uri/gi, "URI");
+}
+
+@Component({})
+export default class CitedMaterials extends Vue {
+  @Prop() mcs!: MaterialCitation[];
+  open = false;
+
+  date(mc:MaterialCitation): string {
+    if (!mc.eventDate) return "";
+    return (new Date(mc.eventDate)).getFullYear().toString();
+  }
+
+  pretty(mc: MaterialCitation): string {
+    let result = "";
+    if (mc.typeStatus) result += `Status: ${mc.typeStatus}\n`;
+    if (mc.recordedBy) result += `Recorded by ${mc.recordedBy}\n`
+    for (const [key, value] of Object.entries(mc)) {
+      if (value && !(["collectionCode", "catalogNumber", "typeStatus", "recordedBy"].includes(key))) {
+        result += `${unCamelCase(key)}: ${value}\n`
+      }
+    }
+    if (result === "") result = "no additional details provided";
+    return result;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+template {
+  position: relative;
+}
+
+button {
+  font-family: "Fira Code", monospace;
+}
+
+.mc_type {
+  font-weight: bold;
+}
+
+.mc_links {
+  a+a::before {
+    content: " ";
+  }
+}
+
+.mc_date {
+  margin-inline: 1em;
+  display: block;
+}
+
+.mc_type {
+  min-width: 1.2em;
+  text-align: center;
+}
+
+.green {
+  color: #388e3c;
+}
+
+table {
+  background: #ffffff;
+  border: 1px solid #00000033;
+  border-radius: 0.2rem;
+  font-size: 0.8rem;
+  max-width: 100%;
+  overflow: auto;
+  width: auto;
+  min-width: 33ch;
+  border-collapse: separate;
+  position: absolute;
+
+  * {
+    background: none;
+    border: none;
+    white-space: nowrap;
+    text-align: left;
+  }
+
+  td {
+    padding: 0;
+  }
+}
+</style>
--- src/components/TreatmentsView.vue
+++ src/components/TreatmentsView.vue
@@ -2,64 +2,32 @@
   <details>
     <summary>
       Treatments
-      (<spinner v-if="js.loading" /><svg
-        v-for="n in js.treatments.def.length"
-        :key="n + 'def'"
-        class="green"
-        viewBox="0 0 24 24"
-      >
-        <path
-          fill="currentcolor"
-          d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
-        />
+      (
+      <spinner v-if="js.loading" /><svg v-for="n in js.treatments.def.length" :key="n + 'def'" class="green"
+        viewBox="0 0 24 24">
+        <path fill="currentcolor"
+          d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
       </svg>
-      <svg
-        v-for="n in js.treatments.aug.length"
-        :key="n + 'aug'"
-        class="blue"
-        viewBox="0 0 24 24"
-      >
-        <path
-          fill="currentcolor"
-          d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
-        />
+      <svg v-for="n in js.treatments.aug.length" :key="n + 'aug'" class="blue" viewBox="0 0 24 24">
+        <path fill="currentcolor" d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
       </svg>
-      <svg
-        v-for="n in js.treatments.dpr.length"
-        :key="n + 'dpr'"
-        class="red"
-        viewBox="0 0 24 24"
-      >
-        <path
-          fill="currentcolor"
-          d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z"
-        />
+      <svg v-for="n in js.treatments.dpr.length" :key="n + 'dpr'" class="red" viewBox="0 0 24 24">
+        <path fill="currentcolor"
+          d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
       </svg>)
     </summary>
     <div v-if="(!js.treatments.def.length && !js.loading) || js.treatments.def.length || js.treatments.aug.length">
       Augmenting Treatments:
       <ul class="nobullet">
-        <li
-          v-for="t in js.treatments.def"
-          :key="t.url"
-        >
-          <svg
-            class="green"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentcolor"
-              d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
-            />
+        <li v-for="t in js.treatments.def" :key="t.url">
+          <svg class="green" viewBox="0 0 24 24">
+            <path fill="currentcolor"
+              d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
           </svg>
           <a :href="t.url">
             {{ t.creators }} ({{ t.date }}) <!--<code>{{ t.url.substring(t.url.indexOf('/id/') + 4) }}</code>-->
           </a>
-          <ul v-if="t.materialCitations.length > 0">
-            <li>
-             Cited materials: <span v-for="mc in t.materialCitations" class="mc" :key="mc.collectionCode + '::' + mc.catalogNumber"><abbr :title="JSON.stringify(mc, undefined, 2)">{{mc.collectionCode}}: {{mc.catalogNumber}}</abbr></span>
-            </li>
-          </ul>
+          <CitedMaterials :mcs="t.materialCitations" />
           <!--<ul v-if="deprecates.find(d => d.url === t.url)">
             <li v-for="d in deprecates.filter(d => d.url === t.url)" :key="d.old">
               Deprecates {{ getFormattedName(d.old) }}
@@ -67,38 +35,20 @@
           </ul>-->
         </li>
         <li v-if="!js.treatments.def.length && !js.loading">
-          <svg
-            class="green"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentcolor"
-              d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z"
-            />
+          <svg class="green" viewBox="0 0 24 24">
+            <path fill="currentcolor"
+              d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z" />
           </svg>
           Defining treatment not yet on Plazi
         </li>
-        <li
-          v-for="t in js.treatments.aug"
-          :key="t.url"
-        >
-          <svg
-            class="blue"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentcolor"
-              d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
-            />
+        <li v-for="t in js.treatments.aug" :key="t.url">
+          <svg class="blue" viewBox="0 0 24 24">
+            <path fill="currentcolor" d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
           </svg>
           <a :href="t.url">
             {{ t.creators }} ({{ t.date }})
           </a>
-          <ul v-if="t.materialCitations.length > 0">
-            <li>
-             Cited materials: <span v-for="mc in t.materialCitations" class="mc" :key="mc.collectionCode + '::' + mc.catalogNumber"><abbr :title="JSON.stringify(mc, undefined, 2)">{{mc.collectionCode}}: {{mc.catalogNumber}}</abbr></span>
-            </li>
-          </ul>
+          <CitedMaterials :mcs="t.materialCitations" />
           <!--<ul v-if="deprecates.find(d => d.url === t.url)">
             <li v-for="d in deprecates.filter(d => d.url === t.url)" :key="d.old">
               Deprecates {{ getFormattedName(d.old) }}
@@ -110,27 +60,15 @@
     <div v-if="js.treatments.dpr.length">
       Deprecating Treatments:
       <ul class="nobullet">
-        <li
-          v-for="t in js.treatments.dpr"
-          :key="t.url"
-        >
-          <svg
-            class="red"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentcolor"
-              d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z"
-            />
+        <li v-for="t in js.treatments.dpr" :key="t.url">
+          <svg class="red" viewBox="0 0 24 24">
+            <path fill="currentcolor"
+              d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
           </svg>
           <a :href="t.url">
             {{ t.creators }} ({{ t.date }}) <!--<code>{{ t.url.substring(t.url.indexOf('/id/') + 4) }}</code>-->
           </a>
-          <ul v-if="t.materialCitations.length > 0">
-            <li>
-             Cited materials: <span v-for="mc in t.materialCitations" class="mc" :key="mc.collectionCode + '::' + mc.catalogNumber"><abbr :title="JSON.stringify(mc, undefined, 2)">{{mc.collectionCode}}: {{mc.catalogNumber}}</abbr></span>
-            </li>
-          </ul>
+          <CitedMaterials :mcs="t.materialCitations" />
           <!--<ul v-if="deprecations.find(d => d.url === t.url)">
             <li v-for="d in deprecations.filter(d => d.url === t.url)" :key="d.new">
               Deprecated by {{ getFormattedName(d.new) }}
@@ -146,9 +84,10 @@
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import type { SyncJustifiedSynonym } from '@/utilities/SynogroupSync'
 import Spinner from '@/components/Spinner.vue'
+import CitedMaterials from './CitedMaterials.vue';
 
 @Component({
-  components: { Spinner }
+  components: { Spinner, CitedMaterials }
 })
 export default class TreatmentView extends Vue {
   @Prop() js!: SyncJustifiedSynonym;
